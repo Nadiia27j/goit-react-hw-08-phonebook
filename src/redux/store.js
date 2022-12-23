ceb120c0412f6eb0f30a0d1be513e0a0d9bdd1fc
Надія Ткач <nadiya77723@gmail.com>
2022-12-23 14:38:14 +0200

    /

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+export const store = configureStore({
+    reducer: {
+      auth: ,
+      contacts: ,
+      filter: ,
+    },
+    middleware(getDefaultMiddleware) {
+      return getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      });
+    }, 
+});
+  
+export const persistedStore = persistStore(store);
