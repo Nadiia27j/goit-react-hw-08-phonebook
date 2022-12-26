b7081e78cb6a420b484c8cf86989f9de0710f751
Надія Ткач <nadiya77723@gmail.com>
2022-12-26 19:59:52 +0200

    add contactList

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+  list-style: none;
+`;
+
+export const Item = styled.li`
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+  display: flex;
+`;
+
+export const ButtonDelete = styled.button`
+  margin-left: 50px;
+  :hover {
+    background-color: gray;
+  }
+`;
