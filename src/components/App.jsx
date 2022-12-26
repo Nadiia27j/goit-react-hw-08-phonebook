a319325db11175c490c39619b454c84e7e464451
Надія Ткач <nadiya77723@gmail.com>
2022-12-26 18:16:41 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/contacts' element={<}></Route>
+        <Route></Route>
+      </Route>
+    </Routes>
   );
 };
+
+<Routes>
+  <Route path="/" element={<Layout />}>
+    <Route index element={<HomePage />}/>
+    <Route
+      path="/register"
+      element={
+        <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+      }
+    />
+    <Route
+      path="/login"
+      element={
+        <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+      }
+    />
+    <Route
+      path="/contacts"
+      element={
+        <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+      }
+    />
+    <Route path="*" element={<NotFound />} />
+  </Route>
+</Routes>;
