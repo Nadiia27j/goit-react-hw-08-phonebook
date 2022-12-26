f7c13d503ae1bc533cbf8b87ab1a009d1f0b01b9
Надія Ткач <nadiya77723@gmail.com>
2022-12-26 22:16:40 +0200

    auth slice почала робити

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,2 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
+
+const initialState = {
+    user: { name: null, email: null },
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+};
